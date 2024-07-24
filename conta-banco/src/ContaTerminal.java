dfb8b403382ae213ff5abe52418433d52f6869a7
lucasqueiro-z <lucas.queiroz001@hotmail.com>
2024-07-24 12:30:44 -0300

    projeto-dio-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero_conta = scanner.nextInt();
+
+        System.out.println("Número da agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+        String nome_cliente = scanner.next();
+
+        System.out.println("Insira o saldo da conta: ");
+        Double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá "+nome_cliente+ ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero_conta+" e seu saldo R$"+saldo+" já está disponível para saque!");
     }
 }
